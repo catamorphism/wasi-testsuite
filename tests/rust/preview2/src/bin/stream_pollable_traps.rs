c64d79f618335ae0ddc8835e98f6618220b8ed0c
Tim Chevalier <tjc@igalia.com>
2025-04-07 15:00:29 -0700

    Add sleep and streams tests from wasmtime

---
+++ tests/rust/preview2/src/bin/stream_pollable_traps.rs
@@ -0,0 +1,10 @@
+use wasi::cli::stdin;
+use wasi::io::streams;
+
+fn main() {
+    let stdin: streams::InputStream = stdin::get_stdin();
+    let stdin_pollable = stdin.subscribe();
+    stdin_pollable.block();
+    drop(stdin);
+    unreachable!("execution should have trapped in line above when stream dropped before pollable");
+}
